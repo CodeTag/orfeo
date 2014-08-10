fc7b1addce5af273b4a5276e954aa8d7fa355187
ignusmart <ignusmart@gmail.com>
2014-08-10 03:17:52 -0500

    Relation User from auth > CustomUser

--- apps/auth/models.py
+++ apps/auth/models.py
@@ -1,6 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 class CustomUser(models.Model):
-  username   = CharField()
-  last_login = DateTimeField(blank=True)
-  is_active  = BooleanField()
+  	user = models.OneToOneField(User)
+  	friends = models.ManyToManyField('self')
+
+  	def __unicode__(self):
+		return self.user.first_name
+
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        CustomUser.objects.create(user=instance)
+
+post_save.connect(create_user_profile, sender=User)
--- apps/playlist/models.py
+++ apps/playlist/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from apps.songs.models import Song
+from apps.auth.models import CustomUser
 
 # Create your models here.
 class Playlist(models.Model):
@@ -7,6 +8,7 @@ class Playlist(models.Model):
 	name = models.CharField(max_length = 50)
 	genre = models.CharField(max_length=50)
 	songs = models.ManyToManyField(Song) 
+	user = models.ForeignKey(CustomUser)
 
 	def __unicode__(self):
 		return self.name
